e899e9893f658ae847ca229a985eabc891af23ac
kushmistry2002 <103053972+kushmistry2002@users.noreply.github.com>
2023-06-10 16:22:21 +0530

    Delete travel_api directory

--- travel_api/src/main/java/com/kush/travel_api/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.kush.travel_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kush.travel_api.entities.User;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-
-}
